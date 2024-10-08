3e76b65e8e6f2268f75fb1c5be63e0f10adfa8ed
Alejandro Romero <alejo8591@gmail.com>
2024-10-07 22:09:51 -0500

    Module_I: Update lab4 with HOC

--- module_I/lab4/src/app/product/layout.jsx
+++ module_I/lab4/src/app/product/layout.jsx
@@ -1,8 +1,17 @@
-export default function Layout({ children }) {
+"use client";
+
+import NavbarHome from "lab4/components/shared/navbar-home/page";
+import Footer from "lab4/components/shared/footer/page";
+import withAuth from "lab4/hoc/with-auth";
+
+function Layout({ children }) {
   return (
     <main>
-      <nav>Product navigation</nav>
-      {children}
+      <NavbarHome/>
+        {children}
+      <Footer/>
     </main>
   )
 }
+
+export default withAuth(Layout);
