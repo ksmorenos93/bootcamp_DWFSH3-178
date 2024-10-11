055f07ff8885ec47c7e2b94a8bc5e6250e3704ee
Alejandro Romero <alejo8591@gmail.com>
2024-10-10 23:20:59 -0500

    Module_I: Update lab9 with redux

--- module_I/lab9/src/app/layout.js
+++ module_I/lab9/src/app/layout.js
@@ -2,6 +2,7 @@ import localFont from "next/font/local";
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 import "./globals.css";
+import StoreProvider from "lab9/store/provider";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -23,7 +24,9 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
+      <StoreProvider>
         {children}
+      </StoreProvider>
       </body>
     </html>
   );
